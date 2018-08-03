a0da7422c93fd33f5e667b3799645d11678e3957
araujothi <42076522+araujothi@users.noreply.github.com>
2018-08-03 17:05:27 -0300

    Add files via upload

---
+++ criptografia cesar.py
@@ -0,0 +1,38 @@
+def criptografar( mensagem, rot):
+    alf = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+      "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z" ]
+
+    n = len(alf)
+    mensagem.lower()
+
+    cifra = " "
+
+
+    for letra in mensagem:
+        if letra in alf:
+            indice = alf.index(letra)
+            nova_letra = alf[(indice + rot)%n]
+            cifra = cifra + nova_letra
+        else:
+            cifra += letra
+    return cifra
+
+def descriptografar( mensagem, rot):
+    return criptografar( mensagem, - rot)
+
+
+arquivo = open("arquivo.txt", "r", encoding="utf8")
+conteudo = arquivo.read()
+arquivo.close
+
+
+rot = 13
+cifra = criptografar(conteudo,rot)
+
+
+
+arquivo = open("mensagem_cifrada.txt", "w")
+arquivo.write(cifra)
+arquivo.close()
+
+print(" arquivo cifrada com sucesso ")
